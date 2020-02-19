d9f795d3de62564445acce5216af48347ba78aa2
fabianamrferreira <fabianamrferreira@gmail.com>
2020-02-19 21:59:20 +0000

    Opção de login pelo Google incompleto

--- src/Web/Startup.cs
+++ src/Web/Startup.cs
@@ -167,6 +167,17 @@ namespace Microsoft.eShopWeb.Web {
             services.AddRazorPages(options => {
                 options.Conventions.AuthorizePage("/Basket/Checkout");
             });
+
+            services.AddAuthentication()
+            .AddGoogle(options =>
+            {
+                IConfigurationSection googleAuthNSection =
+                    Configuration.GetSection("Authentication:Google");
+
+                options.ClientId = googleAuthNSection["ClientId"];
+                options.ClientSecret = googleAuthNSection["ClientSecret"];
+            });
+
             services.AddControllersWithViews();
 
             services.AddHttpContextAccessor();
